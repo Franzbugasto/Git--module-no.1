61635c1ae22acb60f4c118a3ddb24b8eee79ef4c
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-03 22:10:53 +0800

    Create Module.js

---
+++ Module.js
@@ -0,0 +1,32 @@
+// Declaring the variables
+const fullName = "Franz Gunn Mark V. Bugasto";
+let careerGoal = "Programmer";
+const gender = "Male";
+const facebookName = "Franz Gunn Mark V. Bugasto";
+
+// Logging all variables to the console
+console.log("Full Name:", fullName);
+console.log("Career Goal:", careerGoal);
+console.log("Gender:", gender);
+console.log("Facebook Name:", facebookName);
+// Declareing a variable to store my favorite numbers
+let firstFavoriteNumber = 7;  
+let secondFavoriteNumber = 3; 
+let sum = firstFavoriteNumber + secondFavoriteNumber;
+let difference = firstFavoriteNumber - secondFavoriteNumber;
+let product = firstFavoriteNumber * secondFavoriteNumber;
+let quotient = firstFavoriteNumber / secondFavoriteNumber;
+
+// Check (log) both numbers.
+console.log("First Favorite Number:", firstFavoriteNumber);
+console.log("Second Favorite Number:", secondFavoriteNumber);
+
+// Check the power of my 1st favorite number raised to my 2nd favorite number.
+let power = Math.pow(firstFavoriteNumber, secondFavoriteNumber);
+
+// Logging all results to the console
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Power:", power);
